a4ac1a8d0c7cdc88aee666163ffe5902dabd19d9
Taraansh <taraanshnandwani123@gmail.com>
2024-04-15 22:51:56 +0530

    Setup node server

--- home/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Customer
-
-class CustomerSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = ['name', 'address', 'email', 'contact']
--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('endpoints/', views.endpoint, name='endpoint'),
-    path('custname/', views.custname, name='custname'),
-    path('custdetails/<str:name>', views.customer_detail, name='customer_detail')
-]
